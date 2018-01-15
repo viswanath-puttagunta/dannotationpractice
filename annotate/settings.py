4d051ca84f71f1c61c45be42af2e4cd10c73caa6
Viswanath Puttagunta <vishp100@gmail.com>
2018-01-14 19:41:14 -0600

    Split applications. /fabricanno has fabric.js project

--- annotate/settings.py
+++ annotate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'vwebapp',
+    'fabricanno',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ fabricanno/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FabricannoConfig(AppConfig):
+    name = 'fabricanno'
